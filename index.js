af8f90e28af7656fa480da6d14388735c6d273b5
skywolf10 <skywolf10@users.noreply.github.com>
2024-12-16 13:14:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { getDatabase,
          remove } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
 const firebaseConfig = {
-    databaseURL: "https://leads-tracker-app-36771-default-rtdb.firebaseio.com/"
+    databaseURL: "fill-it-in"
 }
 
 const app = initializeApp(firebaseConfig)
